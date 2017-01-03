ebb61ce6a0ee095d179d1bd9a3325f4ba9ebf786
raedhosny <raed.hosny2014@gmail.com>
2017-01-03 14:26:06 +0300

    First Version

--- app/src/main/java/com/tastyhomemade/tastyhomemade/Fragment/SettingsFragment.java
+++ app/src/main/java/com/tastyhomemade/tastyhomemade/Fragment/SettingsFragment.java
@@ -7,16 +7,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.tastyhomemade.tastyhomemade.Others.Settings;
+import com.tastyhomemade.tastyhomemade.Others.Utils;
 import com.tastyhomemade.tastyhomemade.R;
 
 /**
  * Created by Raed on 11/22/2016.
  */
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements View.OnClickListener {
 
-    Button btnsettings_arabic,btnsettings_English;
+    Button btnsettings_arabic,btnsettings_English,btnSave;
+    Settings ObjSettings;
+    boolean IsArabic=false;
 
 
     @Nullable
@@ -30,5 +35,51 @@ public class SettingsFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         btnsettings_arabic =(Button)view.findViewById(R.id.settings_arabic);
         btnsettings_English =(Button)view.findViewById(R.id.settings_English);
+        btnSave = (Button)view.findViewById(R.id.btnSave);
+        btnsettings_arabic.setOnClickListener(this);
+        btnsettings_English.setOnClickListener(this);
+        btnSave.setOnClickListener(this);
+        ObjSettings = new Settings(getContext());
+        if (ObjSettings.getCurrentLanguageId() ==1)
+        {
+            btnsettings_arabic.setBackgroundResource(R.drawable.settings_on);
+            btnsettings_English.setBackgroundResource(R.drawable.settings_off);
+            IsArabic = true;
+        }
+        else if (ObjSettings.getCurrentLanguageId() ==2)
+        {
+            btnsettings_arabic.setBackgroundResource(R.drawable.settings_off);
+            btnsettings_English.setBackgroundResource(R.drawable.settings_on);
+            IsArabic = false;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnsettings_arabic)
+        {
+            btnsettings_arabic.setBackgroundResource(R.drawable.settings_on);
+            btnsettings_English.setBackgroundResource(R.drawable.settings_off);
+            IsArabic =true;
+        }
+        else if (v == btnsettings_English)
+        {
+            btnsettings_arabic.setBackgroundResource(R.drawable.settings_off);
+            btnsettings_English.setBackgroundResource(R.drawable.settings_on);
+            IsArabic =false;
+        }
+        else if (v == btnSave)
+        {
+            if (IsArabic)
+            {
+                Utils.SetCurrentLanguage(getContext(),1);
+                Toast.makeText(, "", Toast.LENGTH_SHORT).show();
+            }
+            else if (!IsArabic)
+            {
+                Utils.SetCurrentLanguage(getContext(),2);
+            }
+
+        }
     }
 }
